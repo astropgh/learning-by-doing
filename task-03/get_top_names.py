23934c7cf5aa6096c2d6fb5e0114f1f47767f240
Kuan Wang <kuanwang@mc-wifi-10-215-118-98.wireless.pitt.edu>
2018-10-12 18:00:44 -0400

    task03 completed

--- task-03/get_top_names.py
+++ task-03/get_top_names.py
@@ -4,16 +4,25 @@ For astrophg/learning-by-doing: Task 3
 https://github.com/astropgh/learning-by-doing/tree/master/task-03
 """
 
-def extract_data_lines(filename, start_text, end_text):
+def extract_data_lines(filename, start_text, end_text, include_start, include_end):
     """
     open `filename`, and yield the lines between
     the line that contains `start_text` and the line that contains `end_text`
     """
     # fill in code as needed
     with open(filename) as fh:
+        on = False
         for line in fh:
-            # fill in code as needed
-            # use `yield line` to return desired lines but keep the function going
+            if start_text in line:
+                on = True
+                if not include_start:
+                    continue
+            if end_text in line and not include_end:
+                    break
+            if on:
+                yield line
+            if end_text in line and include_end:
+                break
 
 
 if __name__ == '__main__':
@@ -21,5 +30,5 @@ if __name__ == '__main__':
     start_text = '<tr><td align="center">2017</td>'
     end_text = '</table></center></div><!-- end #content -->'
 
-    for line in extract_data_lines(filename, start_text, end_text):
+    for line in extract_data_lines(filename, start_text, end_text, include_start=True, include_end=True):
         print(line)
