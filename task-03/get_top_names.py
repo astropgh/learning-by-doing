1084f4f71210e021cc343ad0ba3419f8c9c91dd0
djperrefort <djperrefort@gmail.com>
2018-10-25 21:07:19 -0400

    Completes task-03

--- task-03/get_top_names.py
+++ task-03/get_top_names.py
@@ -4,16 +4,44 @@ For astrophg/learning-by-doing: Task 3
 https://github.com/astropgh/learning-by-doing/tree/master/task-03
 """
 
-def extract_data_lines(filename, start_text, end_text):
+
+def extract_data_lines(
+        filename,
+        start_text,
+        end_text,
+        include_start=False,
+        include_end=False):
+    """Open `filename`, and yield the lines between start and end text
+
+    Args:
+        filename       (str): Path of the file to open
+        start_text     (str): Line in file to start yielding from
+        end_text       (str): Line in file to stop yielding at
+        include_start (bool): Whether to yield start_text (Default false)
+        include_end   (bool): Whether to yield end_text (Default false)
     """
-    open `filename`, and yield the lines between
-    the line that contains `start_text` and the line that contains `end_text`
-    """
-    # fill in code as needed
-    with open(filename) as fh:
-        for line in fh:
-            # fill in code as needed
-            # use `yield line` to return desired lines but keep the function going
+
+    with open(filename) as ofile:
+        stop_iteration = False
+        yield_content = False
+
+        for line in ofile:
+            if stop_iteration:
+                raise StopIteration
+
+            cleaned_line = line.strip()
+            if start_text in cleaned_line:
+                yield_content = True
+                if include_start:
+                    yield cleaned_line
+
+            elif end_text in cleaned_line:
+                stop_iteration = True
+                if include_end:
+                    yield cleaned_line
+
+            elif yield_content:
+                yield cleaned_line
 
 
 if __name__ == '__main__':
@@ -21,5 +49,5 @@ if __name__ == '__main__':
     start_text = '<tr><td align="center">2017</td>'
     end_text = '</table></center></div><!-- end #content -->'
 
-    for line in extract_data_lines(filename, start_text, end_text):
+    for line in extract_data_lines(filename, start_text, end_text, 0, 0):
         print(line)
