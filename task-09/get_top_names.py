fe5eb42f0ff6e3a34c3ab46b065a583544a3cd7f
Kw Huang <kuanweih@andrew.cmu.edu>
2018-11-16 23:48:54 -0500

    done step 2

--- task-09/get_top_names.py
+++ task-09/get_top_names.py
@@ -45,6 +45,15 @@ class NameRecorder:
         self.records.clear()
         self.year = None
 
+    def to_sql(self, filename=None):
+        if filename is None:
+            raise ValueError('One must set a filename!')
+        else:
+            with open(filename, 'w') as file:
+                file.write('year  gender  rank  name\n')
+                for raw in self.records:
+                    file.write("%s  %s  %s     %s\n" %(raw[0], 'female' if raw[1] else 'male  ', raw[2], raw[3]))
+
 
 if __name__ == '__main__':
     filename = '../task-03/top5names.html'
@@ -63,5 +72,7 @@ if __name__ == '__main__':
             recorder.year = int(data_line.replace('<tr><td align="center">', '').strip()[:4])
 
     data = recorder.to_pandas()
+
+    recorder.to_sql('to_sql.txt')
 
     print(data.query('name == "Emma"').query('rank == 1')['year'].tolist())
